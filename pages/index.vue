aba23e9a7df9f4c551328add4fbc6694921df244
Leutrim Nuredini <l.nuredini@beo-kosovo.com>
2020-01-30 16:38:33 -0800

    update home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <template>
-
+    <v-container>
+        <v-layout row wrap>
+           <v-flex xs12 sm6 class="text-xs-center text-sm-right"> 
+             <v-btn large="router" to="/Meetup/meetups" class="primary">Explore Meetups</v-btn>
+           </v-flex>
+           <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+             <v-btn large="router" to="/Meetup/CreateMetups" class="primary">Organize Meetup</v-btn>
+           </v-flex>
+        </v-layout>
+        <v-carousel>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+    </v-container>
 </template>
 
 <script>
@@ -9,7 +27,9 @@ import VuetifyLogo from '~/components/VuetifyLogo.vue'
 export default {
   components: {
     Logo,
-    VuetifyLogo
+    VuetifyLogo,
+
+   
   }
 }
 </script>
